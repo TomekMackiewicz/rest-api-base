279f20cef43fea71cac596f5c2597ff76d145039
Tomasz Mackiewicz <tomasz.mackiewicz79@gmail.com>
2018-06-22 00:26:55 +0200

    Simplify item service

--- frontend/src/app/item/item.service.ts
+++ frontend/src/app/item/item.service.ts
@@ -8,23 +8,23 @@ export class ItemService {
     constructor(private http: HttpClient) {}
 
     getItem(id: number) {
-        return this.http.get('http://localhost:8000/admin/items/' + id);
+        return this.http.get('http://localhost:8000/api/admin/items/' + id);
     }
 
     getItems() {
-        return this.http.get('http://localhost:8000/admin/items')
+        return this.http.get('http://localhost:8000/api/admin/items')
     }
 
     createItem(item: any) {        
-        return this.http.post('http://localhost:8000/admin/items', item);
+        return this.http.post('http://localhost:8000/api/admin/items', item);
     }
 
     updateItem(item: any) {
-        return this.http.patch('http://localhost:8000/admin/items/' + item.id, item);
+        return this.http.patch('http://localhost:8000/api/admin/items/' + item.id, item);
     }
 
     deleteItem(item: any) {
-        return this.http.delete('http://localhost:8000/admin/items/' + item.id);
+        return this.http.delete('http://localhost:8000/api/admin/items/' + item.id);
     }
 
 }

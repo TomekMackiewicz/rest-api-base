ad3cab427aa195a6f3ca9ee47004b3bd01b448a9
Tomasz Mackiewicz <tomasz.mackiewicz79@gmail.com>
2018-06-27 19:16:59 +0200

    Changr password dynamic route

--- frontend/src/app/change-password/change-password.service.ts
+++ frontend/src/app/change-password/change-password.service.ts
@@ -8,8 +8,9 @@ export class ChangePasswordService {
     constructor(private http: HttpClient) {}
 
     changePassword(currentPassword: string, newPassword: string, confirmPassword: string) {
+        let userId = localStorage.getItem('userId');
         return this.http.post(
-            'http://localhost:8000/api/password/1/change', { 
+            'http://localhost:8000/api/password/'+userId+'/change', { 
                 current_password: currentPassword,
                 plainPassword: {
                     first: newPassword,

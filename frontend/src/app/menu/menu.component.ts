e23733d3d56dfefc0f21e96b81df15b5fb07b8d4
Tomek <tomasz.mackiewicz79@gmail.com>
2018-10-18 22:26:46 +0200

    Global alert

--- frontend/src/app/menu/menu.component.ts
+++ frontend/src/app/menu/menu.component.ts
@@ -1,5 +1,4 @@
 import { Component, ChangeDetectorRef } from '@angular/core';
-import { ItemService } from '../item/item.service';
 import { AlertService } from '../alert/alert.service';
 import { LoaderService } from '../services/loader.service';
 
@@ -11,33 +10,12 @@ import { LoaderService } from '../services/loader.service';
 
 export class MenuComponent {
 
-    public categories: any;
-    public forms: any;
-
     constructor(
-        private categoriesService: ItemService,
         private alertService: AlertService,
         private loaderService: LoaderService,
         private ref: ChangeDetectorRef
     ) {}
 
-    ngOnInit() {
-        this.loaderService.displayLoader(true);
-        this.getCategories();
-        this.loaderService.displayLoader(false);
-    }
-
-    getCategories() {
-        this.categoriesService.getItems().subscribe(
-            data => {
-                this.categories = data;
-                this.ref.detectChanges();
-            },
-            error => {
-                this.alertService.error("Error loading categories! " + error);
-                this.ref.detectChanges();
-            }
-        );
-    }
+    ngOnInit() {}
 
 }

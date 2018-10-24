a6eeed7f4f942ea2d4b77075709f55ce358d4c57
Tomek <tomasz.mackiewicz79@gmail.com>
2018-10-24 21:33:01 +0200

    Alert module translations

--- frontend/src/app/alert/alert.module.ts
+++ frontend/src/app/alert/alert.module.ts
@@ -2,10 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlertComponent } from './alert.component';
 import { AlertService } from './alert.service';
+import { HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
+}
 
 @NgModule({
     imports: [
-        CommonModule
+        CommonModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })        
     ],
     declarations: [
         AlertComponent

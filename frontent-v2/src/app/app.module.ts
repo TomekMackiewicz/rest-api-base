22895a8105e794f5adb70aa5c935a0f6dd6fbad7
Tomek <tomasz.mackiewicz79@gmail.com>
2019-03-09 00:06:27 +0100

    Add fonts and icons

--- frontent-v2/src/app/app.module.ts
+++ frontent-v2/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { 
+    MatSidenavModule,
+    MatToolbarModule
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        BrowserAnimationsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
